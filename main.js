efe1372da441527968f85d11dbd91b0e0895b108
Longngok <bboylongngok193@gmail.com>
2023-07-07 20:53:30 +0700

    first commit

--- main.js
+++ main.js
@@ -12,38 +12,31 @@ function togglebar() {
   }
 
 
-  const prevButton = document.getElementById('prevButton');
-  const nextButton = document.getElementById('nextButton');
-  const bannerImages = document.querySelector('.banner-images');
-  const images = bannerImages.querySelectorAll('.banner-img');
-  let currentIndex = 0;
-  
-  // Function to show the current image
-  function showImage(index) {
-    const offset = -index * 100;
-    bannerImages.style.transform = `translateX(${offset}vw)`;
+// Lấy các phần tử cần sử dụng
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+const bannerImages = document.querySelector(".banner-images");
+
+// Định nghĩa biến để theo dõi vị trí hiện tại của ảnh
+let currentPosition = 0;
+
+// Xử lý sự kiện click cho nút Prev
+prevButton.addEventListener("click", () => {
+  currentPosition -= 100; // Điều chỉnh vị trí hiển thị lùi lại 100%
+  if (currentPosition < 0) {
+    currentPosition = 100 * (bannerImages.childElementCount - 1);
   }
-  
-  // Event listener for prevButton
-  prevButton.addEventListener('click', () => {
-    currentIndex--;
-    if (currentIndex < 0) {
-      currentIndex = images.length - 1;
-    }
-    showImage(currentIndex);
-  });
-  
-  // Event listener for nextButton
-  nextButton.addEventListener('click', () => {
-    currentIndex++;
-    if (currentIndex >= images.length) {
-      currentIndex = 0;
-    }
-    showImage(currentIndex);
-  });
-  
-  // Show the initial image
-  showImage(currentIndex);
-  
+  bannerImages.style.transform = `translateX(-${currentPosition}%)`;
+});
+
+// Xử lý sự kiện click cho nút Next
+nextButton.addEventListener("click", () => {
+  currentPosition += 100; // Điều chỉnh vị trí hiển thị tiến lên 100%
+  if (currentPosition > 100 * (bannerImages.childElementCount - 1)) {
+    currentPosition = 0;
+  }
+  bannerImages.style.transform = `translateX(-${currentPosition}%)`;
+});
+
 
   
